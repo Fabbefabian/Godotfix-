d2c82a4f46fdc81b86525ce3a6ad67c8690cf610
Fabian <fabian.persson@hotmail.se>
2022-01-25 14:19:05 +0100

    2022-01-25

--- Gymnasiearbete/fdsad/Action RPG Resources/World/grass.gd
+++ Gymnasiearbete/fdsad/Action RPG Resources/World/grass.gd
@@ -1,10 +1,14 @@
-extends Sprite
+extends Node2D
 
-func _process(delta):
-	if Input.is_action_just_pressed("attack"):
-		var Effect = load("res://fdsad/Action RPG Resources/Effects/GrassEffect.tscn")
-		var grassEffect = Effect.instance()
-		var world = get_tree().current_scene 
-		world.add_child(grassEffect)
-		grassEffect.global_position = global_position
-		queue_free()
+func create_grass_effect():
+	var Effect = load("res://fdsad/Action RPG Resources/Effects/GrassEffect.tscn")
+	var grassEffect = Effect.instance()
+	var world = get_tree().current_scene 
+	world.add_child(grassEffect)
+	grassEffect.global_position = global_position
+	
+
+
+func _on_Hurtbox_area_entered(area):
+	create_grass_effect()
+	queue_free()
