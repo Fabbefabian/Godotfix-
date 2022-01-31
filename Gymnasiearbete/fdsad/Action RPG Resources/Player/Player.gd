e55103d1ffe050079cd6cce10323d43cf510e7ed
Fabian <fabian.persson@hotmail.se>
2022-01-31 14:01:45 +0100

    2022-01-31

--- Gymnasiearbete/fdsad/Action RPG Resources/Player/Player.gd
+++ Gymnasiearbete/fdsad/Action RPG Resources/Player/Player.gd
@@ -2,6 +2,7 @@ extends KinematicBody2D
 
 const ACCELERATION = 500
 const MAX_SPEED =80
+const ROLL_SPEED = 125
 const FRICTION = 500
 
 enum {
@@ -13,7 +14,7 @@ enum {
 
 var state = MOVE
 var velocity = Vector2.ZERO
-var roll_vector = Vector2.DOWN
+var roll_vector = Vector2.LEFT
 
 onready var animationPlayer = $AnimationPlayer
 onready var animationTree = $AnimationTree
@@ -52,7 +53,7 @@ func move_state(delta):
 		animationState.travel("Idle")
 		velocity = velocity. move_toward(Vector2.ZERO, FRICTION * delta)
 	
-	velocity = move_and_slide(velocity)
+	move()
 	
 	if Input.is_action_just_pressed("roll"):
 		state = ROLL
@@ -61,13 +62,20 @@ func move_state(delta):
 	
 	
 func roll_state(delta):
-	velocity = roll_vector * MAX_SPEED
+	velocity = roll_vector * ROLL_SPEED
+	animationState.travel("Roll")
+	move()
 	
 func attack_state(delta):
 	velocity = Vector2.ZERO
 	animationState.travel("Attack")
+	
+	
+func move():
+	velocity = move_and_slide(velocity)
 
 func roll_animation_finished():
+	velocity = Vector2.ZERO
 	state = MOVE
 
 
