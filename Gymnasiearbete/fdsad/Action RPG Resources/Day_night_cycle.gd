60bba87270cd38dd6a4a74f4afe69d109b525205
Fabian <fabian.persson@hotmail.se>
2022-02-02 08:43:29 +0100

    2022-02-01

--- Gymnasiearbete/fdsad/Action RPG Resources/Day_night_cycle.gd
+++ Gymnasiearbete/fdsad/Action RPG Resources/Day_night_cycle.gd
@@ -3,7 +3,7 @@ extends CanvasModulate
 
 func _process(delta):
 	var time = OS.get_time()
-	var TimeInSeconds= time.hour * 3600 + time.minute * 25 + time.second
-	var CurrentFrame = range_lerp(TimeInSeconds,0,360,0,24)
+	var TimeInSeconds= time.hour * 1800 + time.minute * 25 + time.second
+	var CurrentFrame = range_lerp(TimeInSeconds,0,360,0,12)
 	$AnimationPlayer.play("Day_night_cycle")
 	$AnimationPlayer.seek(CurrentFrame)
