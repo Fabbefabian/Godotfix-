7c7cd0d4d548d1dc2f28ea0105fd0727a39e589d
Fabian <fabian.persson@hotmail.se>
2022-02-08 13:48:24 +0100

    2022-02-08

--- Gymnasiearbete/fdsad/Action RPG Resources/Menu.gd
+++ Gymnasiearbete/fdsad/Action RPG Resources/Menu.gd
@@ -8,9 +8,9 @@ func _on_Button_pressed():
 	get_tree(). change_scene("res://fdsad/Action RPG Resources/World.tscn")
 
 
-func _on_OptionsButton_pressed():
-	var options = load("")
-
-
 func _on_QuitButton_pressed():
 	get_tree(). quit()
+
+
+func _on_Options_pressed():
+	var options = load("")
