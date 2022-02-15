17e41bda3d6eed1481182803460353cb02b7d2f6
Fabbefabian <93322990+Fabbefabian@users.noreply.github.com>
2022-02-15 19:40:01 +0100

    yepp

---
+++ Gymnasiearbete/fdsad/Action RPG Resources/Plankor med spik.gd
@@ -0,0 +1,9 @@
+extends TileMap
+
+
+const TILEMAP = 1
+func _on_body_entered(body):
+	body.set_collision_mask_bit(TILEMAP, false)
+
+func _on_body_exited(body):
+	body.set_collision_mask_bit(TILEMAP, true)
