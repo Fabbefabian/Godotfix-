1527da3612c7429cf6dcc17359b06ad326794e0a
Fabian <fabian.persson@hotmail.se>
2022-03-29 13:43:42 +0200

    yepp

--- Gymnasiearbete/fdsad/Action RPG Resources/Player/Player.gd
+++ Gymnasiearbete/fdsad/Action RPG Resources/Player/Player.gd
@@ -5,6 +5,8 @@ export var MAX_SPEED =80
 export var ROLL_SPEED = 125
 export var  FRICTION = 500
 
+
+
 enum {
 	MOVE,
 	ROLL,
@@ -39,27 +41,49 @@ func _physics_process(delta):
 		
 		ATTACK:
 			attack_state(delta)
+		
 
+
+
+
+	
+	
 func move_state(delta):
 	var input_vector = Vector2.ZERO
 	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
 	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
 	input_vector = input_vector.normalized()
 	
-	if input_vector != Vector2.ZERO:
-		roll_vector = input_vector
-		SwordHitbox.knockback_vector = input_vector
-		animationTree.set("parameters/Idle/blend_position", input_vector)
-		animationTree.set("parameters/Run/blend_position", input_vector)
-		animationTree.set("parameters/Attack/blend_position", input_vector)
-		animationTree.set("parameters/Roll/blend_position", input_vector)
-		animationState.travel("Run")
-		velocity = velocity.move_toward(input_vector * MAX_SPEED, ACCELERATION * delta)
-	 
-	else:
-		animationState.travel("Idle")
-		velocity = velocity. move_toward(Vector2.ZERO, FRICTION * delta)
 	
+	if Input.get_action_strength("Sprint") != 0:
+		if input_vector != Vector2.ZERO:
+			roll_vector = input_vector
+			SwordHitbox.knockback_vector = input_vector
+			animationTree.set("parameters/Idle/blend_position", input_vector)
+			animationTree.set("parameters/Run/blend_position", input_vector)
+			animationTree.set("parameters/Attack/blend_position", input_vector)
+			animationTree.set("parameters/Roll/blend_position", input_vector)
+			animationState.travel("Run")
+			velocity = velocity.move_toward(input_vector * 100, ACCELERATION * delta)
+		 
+		else:
+			animationState.travel("Idle")
+			velocity = velocity. move_toward(Vector2.ZERO, FRICTION * delta)
+			
+	elif Input.get_action_strength("Sprint") == 0:
+		if input_vector != Vector2.ZERO:
+			roll_vector = input_vector
+			SwordHitbox.knockback_vector = input_vector
+			animationTree.set("parameters/Idle/blend_position", input_vector)
+			animationTree.set("parameters/Run/blend_position", input_vector)
+			animationTree.set("parameters/Attack/blend_position", input_vector)
+			animationTree.set("parameters/Roll/blend_position", input_vector)
+			animationState.travel("Run")
+			velocity = velocity.move_toward(input_vector * MAX_SPEED, ACCELERATION * delta)
+		 
+		else:
+			animationState.travel("Idle")
+			velocity = velocity. move_toward(Vector2.ZERO, FRICTION * delta)
 	move()
 	
 	if Input.is_action_just_pressed("roll"):
