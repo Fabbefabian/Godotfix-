0a4db08e1f7d6fac89dfe5615c866cad1b14bde2
Fabbefabian <93322990+Fabbefabian@users.noreply.github.com>
2022-04-06 20:56:15 +0200

    yepp

--- Gymnasiearbete/fdsad/Action RPG Resources/World.gd
+++ Gymnasiearbete/fdsad/Action RPG Resources/World.gd
@@ -9,13 +9,14 @@ func _ready():
 	for bridge in get_tree().get_nodes_in_group("Bridges"):
 		bridge.connect("TurnOffWaterCliffs", self, "TurnOffWaterCliffs" )
 		bridge.connect("TurnOnWaterCliffs", self, "TurnOnWaterCliffs" )
+	
+
 	print(Global.door_name)
 	
 	if Global.door_name:
 		var door_node = find_node(Global.door_name)
 		if door_node:
-			$Player.global_position = door_node.global_position
-
+			$YSort/gobbar/Mange.global_position = door_node.global_position
 
 func TurnOffWaterCliffs():
 	WaterCliffs.set_collision_layer_bit(0, false)
@@ -26,11 +27,3 @@ func TurnOnWaterCliffs():
 	WaterCliffs.set_collision_layer_bit(0, true)
 	WaterCliffs.set_collision_mask_bit(0, true)
 
-
-func yes():
-	print(Global.door_name)
-	
-	if Global.door_name:
-		var door_node = find_node(Global.door_name)
-		if door_node:
-			$Player.global_position = door_node.global_position
